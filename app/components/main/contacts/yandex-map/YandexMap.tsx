50e6254ac847aca4c4d670c36bd143ef522cb851
Tiran Krimyan <moneypanda887@gmail.com>
2026-01-01 15:24:59 +0400

    delete yandex metrika

--- app/components/main/contacts/yandex-map/YandexMap.tsx
+++ app/components/main/contacts/yandex-map/YandexMap.tsx
@@ -6,6 +6,7 @@ export const YandexMap = () => {
 				style={{ padding: '30px 0' }}
 				src='https://yandex.ru/map-widget/v1/?um=constructor%3Af81599c7100d1fe1c5d875f65e4b62558a2bce65382b3c3ca25d4e33988db480&amp;source=constructor'
 				width='100%'
+				loading='lazy'
 				height='500'
 			></iframe>
 		</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Vollkorn } from 'next/font/google';
-import Script from 'next/script';
 
 const vollkorn = Vollkorn({ display: 'swap', weight: ['400', '700'], subsets: ['latin'] });
 
@@ -17,50 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='ru'>
-			<body className={vollkorn.className}>
-				{children}
-				<Script
-					id='yandex-metrika'
-					strategy='afterInteractive'
-					dangerouslySetInnerHTML={{
-						__html: `(function (m, e, t, r, i, k, a) {
-									m[i] =
-									m[i] ||
-										function () {
-											(m[i].a = m[i].a || []).push(arguments);
-										};
-									m[i].l = 1 * new Date();
-									for (var j = 0; j < document.scripts.length; j++) {
-										if (document.scripts[j].src === r) {
-											return;
-										}
-									}
-									(k = e.createElement(t)),
-										(a = e.getElementsByTagName(t)[0]),
-										(k.async = 1),
-										(k.src = r),
-										a.parentNode.insertBefore(k, a);
-								})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
-	
-								ym(98795913, 'init', {
-									clickmap: true,
-									trackLinks: true,
-									accurateTrackBounce: true,
-									webvisor: true,
-									ecommerce: 'dataLayer',
-								});`,
-					}}
-				/>
-				<noscript>
-					<div>
-						<img
-							src='https://mc.yandex.ru/watch/97153389'
-							style={{ position: 'absolute', left: '-9999px' }}
-							alt=''
-						/>
-					</div>
-				</noscript>
-			</body>
+			<body className={vollkorn.className}>{children}</body>
 		</html>
 	);
 }
